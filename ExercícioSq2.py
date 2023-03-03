720ecfcf0d0051aab220d3d6309d3eaff7fbe680
Leandro Takafaz <104181629+LeandroTakafaz@users.noreply.github.com>
2023-03-03 12:34:58 -0300

    Add files via upload

---
+++ ExercícioSq2.py
@@ -0,0 +1,10 @@
+'''
+ Exercício Python N°2:
+ Faça um programa em linguagem Python que converta metros para centímetros.
+'''
+
+print('\t--Conversão de Medidas--')
+
+metros = int(input('Informe o valor em metros: '))
+
+print('O valor em centimetros é: ', metros*100,'cm')
