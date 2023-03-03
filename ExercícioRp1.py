afc01d356aea19b99d9778921311ec9c58cef008
Leandro Takafaz <104181629+LeandroTakafaz@users.noreply.github.com>
2023-03-03 12:45:25 -0300

    Add files via upload

---
+++ ExercícioRp1.py
@@ -0,0 +1,20 @@
+"""
+Exercício Python N°3:
+Faça um programa em linguagem Python, que lê um 
+número n e imprime os n primeiros números da sequência de Fibonacci.
+"""
+
+print('\t ---- Sequência de Fibonacci---- ')
+termos = int(input("Digite a quantidade de termos para calcular : "))
+
+#começando com 0 e 1
+num1, num2 = 0, 1
+contador = 0
+
+while contador < termos:
+    num3 = num1 + num2
+    # Atualizando valores
+    num1 = num2
+    num2 = num3
+    contador += 1
+    print(num1)
---
+++ ExercícioRp2.py
@@ -0,0 +1,22 @@
+'''
+Exercício Python Nº1:
+Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual 
+ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+'''
+
+while True:
+    # Ler o nome de usuário e a senha
+    username = input("Digite o nome de usuário: ")
+    password = input("Digite a senha: ")
+    
+    # Verificar se a senha é igual ao nome de usuário
+    if password == username:
+        print("Erro: A senha não pode ser igual ao nome de usuário.")
+        continue  # pedir novamente as informações
+    
+    # Se chegou aqui, as informações são válidas
+    break  # sair do loop
+
+# Mostrar as informações válidas
+print("Nome de usuário:", username)
+print("Senha:", password)
