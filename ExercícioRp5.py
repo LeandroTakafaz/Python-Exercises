f7e6c4217e2af1bdfb67a68ecdbbf39a1a1adb75
Leandro Takafaz <104181629+LeandroTakafaz@users.noreply.github.com>
2023-03-08 15:02:10 -0300

    Add files via upload

--- ExercícioRp5.py
+++ ExercícioRp5.py
@@ -1,4 +1,5 @@
 """
+Exercício Python N°5:
 Escreva um programa que mostre todos os números entre 5 e 100 que são divisíveis por
 7, mas não são múltiplos de 5. Os números obtidos devem ser impressos em sequência.
 """
